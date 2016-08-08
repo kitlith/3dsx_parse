127d36d174549cbf032c5b9b0643657f7c071ab7
Kitlith <kitlith@kitl.pw>
2016-08-08 00:43:06 +0000

    Added part of the code handling reading of relocations. This is by no means finished.

--- source/main.c
+++ source/main.c
@@ -15,15 +15,6 @@ struct header {
     u32 rodata_size;
     u32 data_size;
     u32 bss_size;
-    char rest[];
-};
-
-#pragma pack(1)
-struct extheader {
-    u32 smdh_offset;
-    u32 smdh_size;
-    u32 romfs_offset;
-    char rest[];
 };
 
 #pragma pack(1)
@@ -36,12 +27,16 @@ struct relocation_header {
 struct relocation {
     u16 skip;
     u16 patch;
-    char rest[];
+};
+
+struct relocations { // Yes, I' sorry, naming is hard.
+    struct relocation *absolute;
+    struct relocation *relative;
 };
 
 u8 *buffer;
 
-int main(int argc, char *argv[]) {
+int main(/*int argc, char *argv[]*/) {
     gfxInitDefault();
     consoleInit(GFX_TOP, NULL);
     FILE *file = fopen("test.3dsx", "rb");
@@ -71,24 +66,33 @@ int main(int argc, char *argv[]) {
     printf("Code Size: 0x%08lx\nROData Size: 0x%08lx\nData Size: 0x%08lx\nBSS Size: 0x%08lx\n",
         head->code_size, head->rodata_size, head->data_size, head->bss_size);
 
-    if (head->header_size > 0x20) {
-        struct extheader *ext = (struct extheader*)(head->rest);
-        printf("SMDH offset: 0x%08lx\nSMDH size: 0x%08lx\nRomFS offset: 0x%08lx\n",
-            ext->smdh_offset, ext->smdh_size, ext->romfs_offset);
-    }
-
     const char *reloc_strings[3] = {
         "Code",
         "ROData",
         "Data"
     };
 
+    struct relocation_header *reloc_headers[3];
+
     for (int iii = 0; iii < 3; ++iii) {
         // There are 3 reloaction headers, starting at an offset of the header_size.
         // This is going to descend into a madness of pointer magic, isn't it?
-        struct relocation_header *reloc = (struct reloaction_header*)(buffer + head->header_size + (8 * iii));
+        reloc_headers[iii] = (buffer + head->header_size + (sizeof(struct relocation_header) * iii));
         printf("%s reloaction header\nAbsolute: 0x%08lx\nRelative: 0x%08lx\n",
-            reloc_strings[iii], reloc->absolute, reloc->relative);
+            reloc_strings[iii], reloc_headers[iii]->absolute, reloc_headers[iii]->relative);
+    }
+
+    struct relocations relocs[3];
+
+    for (unsigned int iii = 0; iii < 3; ++iii) {
+        relocs[iii].absolute = (buffer + head->header_size + (sizeof(struct relocation_header) * 3) + head->code_size + head->rodata_size + head->data_size);
+        for (unsigned int jjj = 0; jjj < reloc_headers[iii]->absolute; ++jjj) {
+
+        }
+        relocs[iii].relative = (buffer + head->header_size + (sizeof(struct relocation_header) * 3) + head->code_size + head->rodata_size + head->data_size + (sizeof(struct relocation) * reloc_headers[iii]->absolute));
+        for (unsigned int jjj = 0; jjj < reloc_headers[iii]->relative; ++jjj) {
+
+        }
     }
 
     close:
